4881aeaa0491641cd9e511a72d1ba13826dea98f
Tyler Montobbio <tylermontobbio@gmail.com>
2023-11-09 15:19:10 -0800

    some css and image structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
 import "./App.css";
+import me1 from "./assets/me1.jpg";
 
 function App() {
-  return <div id="app"></div>;
+  return (
+    <div id="app">
+      <div className="container">
+        <div className="indicator">
+          <div className="gallery">
+            <div className="gallery-item">
+              <img src={me1} alt="me1" />
+            </div>
+            <div className="gallery-item">
+              <img src={me1} alt="me1" />
+            </div>
+            <div className="gallery-item">
+              <img src={me1} alt="me1" />
+            </div>
+            <div className="gallery-item">
+              <img src={me1} alt="me1" />
+            </div>
+            <div className="gallery-item">
+              <img src={me1} alt="me1" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
