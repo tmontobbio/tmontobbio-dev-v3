fd2735a6fe12197d02a359d63cdba2ee78300ddd
Tyler Montobbio <tylermontobbio@gmail.com>
2023-11-11 17:28:22 -0800

    first draft of about section

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -8,6 +8,7 @@ export default function About() {
                 <div id="description">
                     <h3>about me</h3>
                     <h4>A highly motivated Front-end Developer from the North Bay 📍</h4>
+                    <p>As a Junior Full-Stack Developer, I possess an impressive arsenal of skills in JavaScript, React, Ruby, Rails, HTML, CSS, . I excel in designing and maintaining responsive websites that offer a smooth user experience. My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimized code and utilizing cutting-edge development tools and techniques. I am also a team player who thrives in collaborating with cross-functional teams to produce outstanding web applications.</p>
                 </div>
             </div>
         </div>
