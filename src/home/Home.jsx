ec8d332c6206d8ec7f2d4108bfeb7a687adfd091
Tyler Montobbio <tylermontobbio@gmail.com>
2023-11-16 13:07:35 -0800

    fixed about container css

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -14,31 +14,33 @@ import "../App.css"
 export default function Home() {
     return (
         <div id="home">
-            <div className="greeting">
-                <div className="greeting-text">
-                    <h1>Full-Stack React Developer</h1>
-                    <p>Hi I&apos;m Tyler, a passionate web developer from the San Francisco bay area.</p>
-                    <div>
-                        <a href="https://github.com/tmontobbio" target="_blank" rel="noreferrer">
-                            <FiGithub className="icon" />
-                        </a>
-                        <a href="https://www.linkedin.com/in/tmontobbio" target="_blank" rel="noreferrer">
-                            <PiLinkedinLogoBold className="icon" />
-                        </a>
+            <div className="container">
+                <div className="greeting">
+                    <div className="greeting-text">
+                        <h1>Full-Stack React Developer</h1>
+                        <p>Hi I&apos;m Tyler, a passionate web developer from the San Francisco bay area.</p>
+                        <div>
+                            <a href="https://github.com/tmontobbio" target="_blank" rel="noreferrer">
+                                <FiGithub className="icon" />
+                            </a>
+                            <a href="https://www.linkedin.com/in/tmontobbio" target="_blank" rel="noreferrer">
+                                <PiLinkedinLogoBold className="icon" />
+                            </a>
+                        </div>
+                        <div className="tech-stack">
+                            <p className="tech-stack-header">Tech Stack  | </p>
+                            <JavaScript className="icon" />
+                            <React className="icon" />
+                            <Redux className="icon" />
+                            <NodeJS className="icon" />
+                            <Ruby className="icon" />
+                            <Rails className="icon" />
+                            <PostGreSQL className="icon" />
+                        </div>
                     </div>
-                    <div className="tech-stack">
-                        <p className="tech-stack-header">Tech Stack  | </p>
-                        <JavaScript className="icon" />
-                        <React className="icon" />
-                        <Redux className="icon" />
-                        <NodeJS className="icon" />
-                        <Ruby className="icon" />
-                        <Rails className="icon" />
-                        <PostGreSQL className="icon" />
-                    </div>
+                    <div className="greeting-img" />
                 </div>
-                <div className="greeting-img" />
             </div>
-        </div >
+        </div>
     )
 }
