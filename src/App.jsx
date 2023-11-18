780aa1a6621c946b6b486851234050e7e6796ba0
tyler <tylermontobbio@gmail.com>
2023-11-17 19:08:30 -0800

    working on hover state and animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css'
+import { useState } from 'react'
+
 import Nav from './nav/Nav.jsx'
 import Home from "./home/Home.jsx"
 import About from "./about/About.jsx"
 import Projects from "./projects/Projects.jsx"
 import Contact from "./contact/Contact.jsx"
 
+import './App.css'
+
 function App() {
+  const [hover, setHover] = useState(false);
+
   return (
     <>
       <Nav />
