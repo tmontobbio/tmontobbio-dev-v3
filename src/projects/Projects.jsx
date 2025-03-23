afb101951f53fd6e0063a1919e8a7e3228c490c1
Tyler Montobbio <tylermontobbio@gmail.com>
2025-03-23 11:27:20 -0700

    broken projects

--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -9,101 +9,37 @@ import montobbioDesign from "../img/montobbio-design.png"
 
 import "../App.css"
 
+
+
+
 export default function Projects() {
 
     return (
         <div id="projects">
-            <h3>PORTFOLIO</h3>
-            <h4>Each of my projects showcases a different aspect of my abilities 🛠️</h4>
-            {/* {sites.map((s) => {
+
+            {/* {
+                sites.map((s) => {
                     return (
                         <Project key={s.title} title={s.title} img={s.img} description={s.description} url={s.url} />
                     )
-                })} */}
+                })
+            } */}
 
+            <h3>PORTFOLIO</h3>
+            <h4>Each of my projects showcases a different aspect of my abilities 🛠️</h4>
             <div className="project">
                 <a href="https://tmontobbio.dev/" target="_blank" rel="noreferrer">
                     <div id='montobbio-design' alt="montobbio design thumbnail" />
                 </a>
-                <div className="txt-wrapper">
-                    <span>
-                        <h5>Montobbio Design</h5>
-                        <p>(2023)</p>
-                    </span>
+                <span>
+                    <h5>tmontobbio.dev</h5>
+                    <p>(2023)</p>
                     <p>The website you&apos;re visiting now! I built this site as a portfolio to showcase my work.  I wanted to keep it simple and fun, so there&apos;s no frills, however I hope it shows how I&apos;ve learned to structure my work over time.</p>
-                    <div className="links">
-                        <a href="https://github.com/tmontobbio" target="_blank" rel="noreferrer">
-                            <div className="btn-container project-btn btn">
-                                Code
-                                <FiGithub className="icon" />
-                            </div>
-                        </a>
-                    </div>
-                </div>
+                    <a href="https://github.com/tmontobbio" target="_blank" rel="noreferrer">
+                        <FiGithub className="icon" />
+                    </a>
+                </span>
             </div>
-
-            {/* <div className="project project-reversed">
-                <a href="https://tmontobbio.dev/" target="_blank" rel="noreferrer">
-                    <img src={montobbioDesign} alt="montobbio design thumbnail" />
-                </a>
-                <div className="txt-wrapper">
-                    <span>
-                        <h5>Montobbio Design</h5>
-                        <p>(2023)</p>
-                    </span>
-                    <p>The very website you&apos;re visiting now! I built this site as a portfolio to showcase my work.  I wanted to keep it simple and fun, so there&apos;s no frills, however I hope it shows how I&apos;ve learned to structure my work over time.</p>
-                    <div className="links">
-                        <a href="https://github.com/tmontobbio" target="_blank" rel="noreferrer">
-                            <div className="btn-container project-btn btn">
-                                Code
-                                <FiGithub className="icon" />
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-            <div className="project">
-                <a href="https://tmontobbio.dev/" target="_blank" rel="noreferrer">
-                    <img src={montobbioDesign} alt="montobbio design thumbnail" />
-                </a>
-                <div className="txt-wrapper">
-                    <span>
-                        <h5>Montobbio Design</h5>
-                        <p>(2023)</p>
-                    </span>
-                    <p>The very website you&apos;re visiting now! I built this site as a portfolio to showcase my work.  I wanted to keep it simple and fun, so there&apos;s no frills, however I hope it shows how I&apos;ve learned to structure my work over time.</p>
-                    <div className="links">
-                        <a href="https://github.com/tmontobbio" target="_blank" rel="noreferrer">
-                            <div className="btn-container project-btn btn">
-                                Code
-                                <FiGithub className="icon" />
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-            <div className="project project-reversed">
-                <a href="https://tmontobbio.dev/" target="_blank" rel="noreferrer">
-                    <img src={montobbioDesign} alt="montobbio design thumbnail" />
-                </a>
-                <div className="txt-wrapper">
-                    <span>
-                        <h5>Montobbio Design</h5>
-                        <p>(2023)</p>
-                    </span>
-                    <p>The very website you&apos;re visiting now! I built this site as a portfolio to showcase my work.  I wanted to keep it simple and fun, so there&apos;s no frills, however I hope it shows how I&apos;ve learned to structure my work over time.</p>
-                    <div className="links">
-                        <a href="https://github.com/tmontobbio" target="_blank" rel="noreferrer">
-                            <div className="btn-container project-btn btn">
-                                Code
-                                <FiGithub className="icon" />
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div> */}
         </div>
     )
 }
