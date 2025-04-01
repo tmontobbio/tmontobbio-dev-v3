5be2b5943a64faad5278572ec010558eae6509cf
Tyler Montobbio <tylermontobbio@gmail.com>
2025-03-31 20:38:30 -0700

    home css desktop

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -15,27 +15,29 @@ export default function Home() {
     return (
         <div id='home' className="container">
             <div id="greeting-img" />
-            <span id="greeting-text">
-                <h1>Full-Stack Developer</h1>
-                <p>Hi I&apos;m Tyler, a passionate web developer from the San Francisco bay area.</p>
-            </span>
-            <div className="btn-container">
-                <p className="tech-stack-header">Tech Stack  | </p>
-                <JavaScript className="icon ts" />
-                <React className="icon ts" />
-                <Redux className="icon ts" />
-                <NodeJS className="icon ts" />
-                <Ruby className="icon ts" />
-                <Rails className="icon ts" />
-                <PostGreSQL className="icon ts" />
-            </div>
-            <div className="btn-container">
-                <a href="https://github.com/tmontobbio" target="_blank" rel="noreferrer">
-                    <FiGithub className="icon btn" />
-                </a>
-                <a href="https://www.linkedin.com/in/tmontobbio" target="_blank" rel="noreferrer">
-                    <PiLinkedinLogoBold className="icon btn" />
-                </a>
+            <div id="home-wrap">
+                <span id="greeting-text">
+                    <h1>Full-Stack Developer</h1>
+                    <p>Hi I&apos;m Tyler, a passionate web developer from the San Francisco bay area.</p>
+                </span>
+                <div className="btn-container">
+                    <p className="tech-stack-header">Tech Stack  | </p>
+                    <JavaScript className="icon ts" />
+                    <React className="icon ts" />
+                    <Redux className="icon ts" />
+                    <NodeJS className="icon ts" />
+                    <Ruby className="icon ts" />
+                    <Rails className="icon ts" />
+                    <PostGreSQL className="icon ts" />
+                </div>
+                <div className="btn-container">
+                    <a href="https://github.com/tmontobbio" target="_blank" rel="noreferrer">
+                        <FiGithub className="icon btn" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/tmontobbio" target="_blank" rel="noreferrer">
+                        <PiLinkedinLogoBold className="icon btn" />
+                    </a>
+                </div>
             </div>
         </div>
     )
