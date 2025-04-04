e48c658448f0367014475c8d12ab43a979e63837
Tyler Montobbio <tylermontobbio@gmail.com>
2025-04-04 12:55:14 -0700

    contact css

--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -42,7 +42,7 @@ export default function Projects() {
                 </div>
             </div>
 
-            <div className="project">
+            <div className="project odd">
                 <a href="https://tmontobbio.dev/" target="_blank" rel="noreferrer">
                     <div className='project-img' alt="montobbio design thumbnail" />
                 </a>
@@ -57,6 +57,20 @@ export default function Projects() {
             </div>
 
             <div className="project">
+                <a href="https://tmontobbio.dev/" target="_blank" rel="noreferrer">
+                    <div className='project-img' alt="montobbio design thumbnail" />
+                </a>
+                <div className="description">
+                    <h5>tmontobbio.dev</h5>
+                    <p>(2023)</p>
+                    <p>The website you&apos;re visiting now! I built this site as a portfolio to showcase my work.  I wanted to keep it simple and fun, so there&apos;s no frills, however I hope it shows how I&apos;ve learned to structure my work over time.</p>
+                    <a href="https://github.com/tmontobbio" target="_blank" rel="noreferrer">
+                        <FiGithub className="icon" />
+                    </a>
+                </div>
+            </div>
+
+            <div className="project odd">
                 <a href="https://tmontobbio.dev/" target="_blank" rel="noreferrer">
                     <div className='project-img' alt="montobbio design thumbnail" />
                 </a>
